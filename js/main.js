2b14f9366944d15a534f82c356251c8d735290d0
Sereyrithy Vandy <vandysereyrithyy@gmail.com>
2023-10-28 15:11:05 +0700

    last

--- js/main.js
+++ js/main.js
@@ -4,29 +4,6 @@ window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 0)
 });
 
-//Portfolio section - Modal
-const portfolioModals = document.querySelectorAll(".portfolio-model");
-const imgCards = document.querySelectorAll(".img-card");
-const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
-
-let portfolioModal = function (modalClick) {
-    portfolioModals[modalClick].classList.add("active");
-}
-
-imgCards.forEach((imgCard, i) => {
-    imgCard.addEventListener("click", () => {
-        portfolioModal(i);
-    })
-})
-
-portfolioCloseBtns.forEach((portfolioCloseBtn) => {
-    portfolioCloseBtn.addEventListener("click", () => {
-        portfolioModals.forEach((portfolioModalView) => {
-            portfolioModalView.classList.remove("active")
-        })
-    })
-})
-
 //Our clients - Swiper
 
 //Website dark/light theme
